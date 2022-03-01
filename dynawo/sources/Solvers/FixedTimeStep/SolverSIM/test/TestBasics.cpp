f0f118343e523ad7c1144a1af994c6872ca8cf92
Gautier Bureau <gautier.bureau@gmail.com>
2022-03-01 10:49:01 +0100

    #2190 Add order 1 prediction in SIM solver.

--- dynawo/sources/Solvers/FixedTimeStep/SolverSIM/test/TestBasics.cpp
+++ dynawo/sources/Solvers/FixedTimeStep/SolverSIM/test/TestBasics.cpp
@@ -996,9 +996,10 @@ TEST(ParametersTest, testParameters) {
   params->addParameter(parameters::ParameterFactory::newParameter("optimizeReinitAlgebraicResidualsEvaluations", false));
   params->addParameter(parameters::ParameterFactory::newParameter("skipNRIfInitialGuessOK", false));
   params->addParameter(parameters::ParameterFactory::newParameter("minimumModeChangeTypeForAlgebraicRestoration", std::string("ALGEBRAIC_J_UPDATE")));
+  params->addParameter(parameters::ParameterFactory::newParameter("order1Prediction", false));
   ASSERT_NO_THROW(solver->setParametersFromPARFile(params));
   ASSERT_NO_THROW(solver->setSolverParameters());
-  ASSERT_EQ(solver->getParametersMap().size(), 40);
+  ASSERT_EQ(solver->getParametersMap().size(), 41);
 }
 
 TEST(ParametersTest, testParametersInit) {
@@ -1046,7 +1047,7 @@ TEST(ParametersTest, testParametersInit) {
   params->addParameter(parameters::ParameterFactory::newParameter("printflAlgInit", 0));
   ASSERT_NO_THROW(solver->setParametersFromPARFile(params));
   ASSERT_NO_THROW(solver->setSolverParameters());
-  ASSERT_EQ(solver->getParametersMap().size(), 40);
+  ASSERT_EQ(solver->getParametersMap().size(), 41);
 }
 
 }  // namespace DYN
