563249f4f8ccb4f9ff3291388667b5eee9ec0951
Dimitri Baron <dimitri.baron@rte-france.com>
2022-11-21 18:45:38 +0100

    #2304 localInitParameters initialization in ModelNetwork

--- dynawo/sources/Models/CPP/ModelNetwork/DYNModelNetwork.cpp
+++ dynawo/sources/Models/CPP/ModelNetwork/DYNModelNetwork.cpp
@@ -1221,7 +1221,7 @@ ModelNetwork::initParams() {
   // otherwise, the network is not balanced, and the global init of the model would be necessary to compute switches currents
   SolverKINSubModel solver;
 
-  boost::shared_ptr<LocalInitParameters> localInitParameters;
+  boost::shared_ptr<LocalInitParameters> localInitParameters = boost::make_shared<LocalInitParameters>();
   localInitParameters->mxiter = 5;
 
   solver.init(this, 0, &yLocalInit_[0], &fLocalInit_[0], localInitParameters);
